1547c564e3cc5f920e009afae5607878469e86a7
Ghostz <davimarcio532@gmail.com>
2024-04-10 17:01:11 -0300

    :sparkles: adding add edit and remove card function

--- script.js
+++ script.js
@@ -1,3 +1,133 @@
+const todoForm = document.querySelector("#todoForm")
+const todoInput = document.querySelector("#todoInput")
+const todoList = document.querySelector("#todoList")
+const editForm = document.querySelector("#editForm")
+const editInput = document.querySelector("#editInput")
+const cancelEditBtn = document.querySelector("#cancelEditBtn")
+
+let oldInputValue
+
+const saveTodo = (text) => {
+
+  const todo = document.createElement("div")
+  todo.classList.add("card")
+
+  const todoTitle = document.createElement("p")
+  todoTitle.innerText = text
+  todo.appendChild(todoTitle)
+  
+  const editBtn = document.createElement("button")
+  editBtn.classList.add("editTodo")
+  editBtn.innerHTML = '<ion-icon name="cut-outline"></ion-icon>'
+  todo.appendChild(editBtn)
+
+  todoList.appendChild(todo)
+
+  const removeBtn = document.createElement("button")
+  removeBtn.classList.add("removeTodo")
+  removeBtn.innerHTML = '<ion-icon name="trash-bin-outline"></ion-icon>'
+  todo.appendChild(removeBtn)
+
+  todoList.appendChild(todo)
+
+  todoInput.value = ""
+  todoInput.focus()
+}
+
+const toggleForms = () => {
+  modal.style.display = "block"
+  editForm.classList.toggle("hide")
+  todoForm.classList.toggle("hide")
+}
+
+const updateTodo = (text) => {
+
+  const todos = document.querySelectorAll(".todo")
+
+  todos.forEach((todo) => {
+
+    let todoTitle = todo.querySelector("p")
+
+    if(todoTitle.innerText === oldInputValue) {
+      todoTitle.innerText = text
+    }
+  })
+}
+
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+
+  const inputValue = todoInput.value
+
+  if(inputValue) {
+    saveTodo(inputValue)
+  }
+})
+
+document.addEventListener("click", (e) => {
+  const targetE1 = e.target
+  const parentE1 = e.target.closest("div")
+  let todoTitle
+
+  if(parentE1 && parentE1.querySelector("p")) {
+    todoTitle = parentE1.querySelector("p").innerText
+  }
+
+
+  if(targetE1.classList.contains("editTodo")) {
+    toggleForms()
+
+    editInput.value = todoTitle
+    oldInputValue = todoTitle
+  }
+
+  if (targetE1.classList.contains("removeTodo")) {
+    parentE1.remove()
+  }
+})
+
+cancelEditBtn.addEventListener("click", (e) => {
+  e.preventDefault()
+
+  toggleForms()
+})
+
+editForm.addEventListener("submit", (e) => {
+
+  e.preventDefault()
+
+  const editInputValue = editInput.value
+
+  if(editInputValue) {
+    updateTodo(editInputValue)
+  }
+
+  toggleForms()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Get the modal
 let modal = document.getElementById("myModal");
 
